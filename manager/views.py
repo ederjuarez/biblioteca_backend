b327ad74214767d861a7019c1f0f85372552445c
Eder Juarez <eder.juarez@ingesoft.com.mx>
2026-03-09 18:04:00 -0600

    filtros

--- manager/views.py
+++ manager/views.py
@@ -10,8 +10,18 @@ from rest_framework.permissions import AllowAny
 class AuthorViewSet(viewsets.ModelViewSet):
     # pagination_class = None
     # permission_classes = [AllowAny]
-    queryset = Author.objects.all().order_by("-pk")
+    # queryset = Author.objects.all().order_by("-pk")
     serializer_class = AuthorSerializer
+
+    def get_queryset(self):
+        data_filer = self.request.query_params.get("data_filer", None)
+        queryset = Author.objects.all().order_by("-pk")
+        for value in data_filer:
+            if value == "name":
+                queryset = queryset.filter(name=value)
+            if value == "birth_date":
+                queryset = queryset.filter(birth_date=value)
+        return queryset
 
 
     @action(detail=False, methods=["get"])
