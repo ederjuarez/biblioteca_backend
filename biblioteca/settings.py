772e2251e3a0504ed4b87998bb0a9e428bac5e93
Carlos Vicente Leyva Alvarez <leyva_65@hotmail.com>
2026-03-11 13:59:52 -0600

    feat: add django_filters

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -72,6 +72,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt",
     "corsheaders",
+    "django_filters",
 ]
 
 MIDDLEWARE = [
--- manager/views.py
+++ manager/views.py
@@ -4,7 +4,8 @@ from .serializers import AuthorSerializer, BookSerializer
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
-
+from rest_framework.permissions import AllowAny
+from django_filters.rest_framework import DjangoFilterBackend
 
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
@@ -20,3 +21,7 @@ class AuthorViewSet(viewsets.ModelViewSet):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [AllowAny]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["author_id"]
+
