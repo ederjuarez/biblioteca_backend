39a2183b0e4ad7fa18da2547c73380ef9cc40e19
Carlos Vicente Leyva Alvarez <leyva_65@hotmail.com>
2026-03-11 09:41:59 -0600

    feat: comentado vista books, permission_classes allowany

--- manager/views.py
+++ manager/views.py
@@ -32,8 +32,8 @@ class AuthorViewSet(viewsets.ModelViewSet):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all().order_by("pk")
     serializer_class = BookSerializer
-    permission_classes = [AllowAny]
-  
+    # permission_classes = [AllowAny]
+
     @action(detail=False, methods=["get"])
     def books_author(self, request, author_id):
         queryset = self.get_queryset().filter(author=author_id)
